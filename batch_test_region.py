74ed61e82286ec31a69d4075d98214759bc25eaf
truongnn1106 <nhattruong.nguyen@vucar.net>
2023-11-30 10:31:01 +0700

    optimize inference time

--- batch_test_region.py
+++ batch_test_region.py
@@ -27,9 +27,20 @@ parser.add_argument(
 
 
 if __name__ == "__main__":
+    
+    x1 = 432
+    y1 = 348
+    x2 = 600
+    y2 = 420
+    
+    # Calculate width and height of the region of interest
+    w_r = x2 - x1
+    h_r = y2 - y1
+    
+    
     FLAGS = ng.Config('inpaint.yml')
     # USE GPU
-    ng.get_gpus(1, dedicated=False)
+    ng.get_gpus(0, dedicated=False)
     os.environ['CUDA_VISIBLE_DEVICES'] ='0'
     
     args = parser.parse_args()
@@ -40,7 +51,7 @@ if __name__ == "__main__":
 
     model = InpaintCAModel()
     input_image_ph = tf.placeholder(
-        tf.float32, shape=(1, args.image_height, args.image_width*2, 3))
+        tf.float32, shape=(1, h_r, w_r*2, 3))
     
     output = model.build_server_graph(FLAGS, input_image_ph)
     output = (output + 1.) * 127.5
@@ -66,32 +77,48 @@ if __name__ == "__main__":
         base = os.path.basename(mask)
 
         image = cv2.imread(image)
+        ori_image = image.copy()
+        
         mask = cv2.imread(mask)
         
-        image = cv2.resize(image, (args.image_width, args.image_height))
-        mask = cv2.resize(mask, (args.image_width, args.image_height))
+        cropped_image = image[y1:y2, x1:x2, :]
+        cropped_mask = mask[y1:y2, x1:x2, :]
+        
+        print('Shape of cropped image: {}'.format(cropped_image.shape))
+        print('Shape of cropped mask: {}'.format(cropped_mask.shape))
+        
+        cropped_image = cv2.resize(cropped_image, (w_r, h_r))
+        cropped_mask = cv2.resize(cropped_mask, (w_r, h_r))
         
         # cv2.imwrite(out, image*(1-mask/255.) + mask)
         # # continue
         # image = np.zeros((128, 256, 3))
         # mask = np.zeros((128, 256, 3))
 
-        assert image.shape == mask.shape
+        assert cropped_image.shape == cropped_mask.shape
 
-        h, w, _ = image.shape
+        h, w, _ = cropped_image.shape
         grid = 4
-        image = image[:h//grid*grid, :w//grid*grid, :]
-        mask = mask[:h//grid*grid, :w//grid*grid, :]
-        print('Shape of image: {}'.format(image.shape))
+        cropped_image = cropped_image[:h//grid*grid, :w//grid*grid, :]
+        cropped_mask = cropped_mask[:h//grid*grid, :w//grid*grid, :]
+        print('Shape of image: {}'.format(cropped_image.shape))
 
-        image = np.expand_dims(image, 0)
-        mask = np.expand_dims(mask, 0)
-        input_image = np.concatenate([image, mask], axis=2)
-
+        cropped_image = np.expand_dims(cropped_image, 0)
+        cropped_mask = np.expand_dims(cropped_mask, 0)
+        
+        input_image = np.concatenate([cropped_image, cropped_mask], axis=2)
+        
+        print(input_image.shape)
         # load pretrained model
         result = sess.run(output, feed_dict={input_image_ph: input_image})
         
+        # Blend the result with the original image with x1, y1, x2, y2
+        
+        ori_image[y1:y2, x1:x2, :] = result[0][:, :, ::-1]
+        result = ori_image
+        
         print('Processed: {}'.format(out))
-        cv2.imwrite(out, result[0][:, :, ::-1])
+        cv2.imwrite(out, result)
+        
 
     print('Time total: {}'.format(time.time() - t))
